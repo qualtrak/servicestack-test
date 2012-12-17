60b9a0b89cb5ebd97ef25535d132ddd1fdc2ca29
Kornelije Sajler <xajler@gmail.com>
2012-12-18 00:58:41 +0100

    Initial Commit. Fully simplistic ServiceStack app, the ASP.NET MVC excluded from it.

---
+++ WebApp/Global.asax.cs
@@ -0,0 +1,37 @@
+﻿namespace WebApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Routing;
+    using System.Web.Security;
+    using System.Web.SessionState;
+
+    public class Global : System.Web.HttpApplication
+    {
+        //public static void RegisterRoutes(RouteCollection routes)
+        //{
+        //    routes.IgnoreRoute("api/{*pathInfo}");
+        //    routes.IgnoreRoute("{*favicon}", new { favicon = @"(.*/)?favicon.ico(/.*)?" }); //Prevent exceptions for favicon
+        //}
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            //RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_BeginRequest(object src, EventArgs e)
+        {
+            if (Request.IsLocal)
+            {
+                ServiceStack.MiniProfiler.Profiler.Start();
+            }
+        }
+
+        protected void Application_EndRequest(object src, EventArgs e)
+        {
+            ServiceStack.MiniProfiler.Profiler.Stop();
+        }
+    }
+}
